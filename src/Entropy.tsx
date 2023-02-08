97aff1f7ecbfcc8d8e5d3a3cd9fe3cf90b626b22
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-02-08 15:01:10 -0300

    refactor(entropyPage): remove unused variables

--- src/Entropy.tsx
+++ src/Entropy.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
 import './App.css'
-import Latex from 'react-latex'
 
 function Entropy() {
-  const [edges, setEdges] = useState(0)
-  const [nodes, setNodes] = useState(0)
-  const [connectedComponents, setConnectedComponents] = useState(0)
-
   return (
     <div className="App">
       <h1>Entropy</h1>
